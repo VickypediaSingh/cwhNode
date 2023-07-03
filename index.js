607772f0a68781db55e626655e271556a6aefeb1
Bikramjeet Singh <officialbikramjeet@gmail.com>
2023-07-04 01:21:18 +0530

    cwh IC

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("jai shree ram")
+
+const fs = require('node:fs/promises');
+
+const os = require ('os');
+const harry = require("./second")
+console.log("hello" ,harry)
+console.log(os.freemem())
---
+++ second.js
@@ -0,0 +1,9 @@
+ 
+
+harry ={
+    name:"harry",
+    favNum:36,
+    developer:true
+}
+console.log(exports,require,module,__filename,__dirname)
+module.exports=harry
